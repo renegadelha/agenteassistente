b3dcfe152542b0c873bfa66116df66cfe2b23bf2
renegadelha <renegadelha@gmail.com>
2026-09-12 09:59:33 -0300

    versao 5 usando edresson model

--- tocar_som.py
+++ tocar_som.py
@@ -1,6 +1,8 @@
 import subprocess
 
-MODELO_PATH = "voices/pt_BR-edresson-low.onnx"
+MODELO_PATH = "voices/pt_BR-faber-medium.onnx"
+#MODELO_PATH = "voices/pt_BR-edresson-low.onnx"
+
 
 def falar_resposta(texto):
     """Gera e reproduz áudio via streaming em tempo real com alta performance"""
@@ -9,31 +11,35 @@ def falar_resposta(texto):
 
     print("[TTS]: Sintetizando e reproduzindo áudio instantaneamente...")
 
+    # --output-raw joga o fluxo de áudio puro direto na memória sem headers
+    # --length-scale 0.9 (opcional) acelera levemente a velocidade da fala para dar mais agilidade
     comando_piper = [
         "python3", "-m", "piper",
         "--model", MODELO_PATH,
         "--output_raw",
-        "--length_scale", "1" # Ajustado para underline caso o traço falhe
+        "--length-scale", "1"
     ]
 
+    # Configuramos o aplay para o formato padrão do Piper (22050Hz, 16-bit, Mono)
+    # Isso faz o som disparar de imediato na caixa USB
     comando_aplay = [
         "aplay", "-q",
-        "-t", "raw",      # AVISA O APLAY QUE É ÁUDIO BRUTO SEM CABEÇALHO WAV
-        "-r", "16000",
+        "-r", "22050",
         "-f", "S16_LE",
-        "-c", "1",
-        "-"               # FORÇA A LEITURA DO STDIN
+        "-c", "1"
     ]
 
+
     try:
-        # ATENÇÃO: Removi o DEVNULL do stderr do Piper.
-        # Se o Piper der erro (ex: falta do .json), ele vai gritar no terminal agora!
+        # Abre o processo do Piper
         piper_proc = subprocess.Popen(
             comando_piper,
             stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL
         )
 
+        # Abre o aplay conectado diretamente à saída do Piper
         aplay_proc = subprocess.Popen(
             comando_aplay,
             stdin=piper_proc.stdout,
